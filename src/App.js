bc18e7ec8944e8f9d0c2ef83e95fa3ad807e8cbf
agaoldziej <agnieszka.oldziej@gmail.com>
2021-01-07 11:55:14 +0100

    Switched code to proper files

--- src/App.js
+++ src/App.js
@@ -1,2 +1,35 @@
-import React, { useState, useEffect } from "react";
-import table from "./table";
+
+import React, { useMemo, useState, useEffect } from "react";
+import Table from "./Table";
+
+  function App() {
+    const columns = useMemo(
+      () => [
+        {
+          // first group - TV Show
+          Header: "Score table",
+          // First group columns
+          columns: [
+            {
+              Header: "Name",
+              accessor: "show.name"
+            },
+            {
+              Header: "Score",
+              accessor: "show.score"
+            }
+          ]
+        },
+        
+      ],
+      []
+    );
+  
+    return (
+      <div className="App">
+        <Table columns={columns} data={data} />
+      </div>
+    );
+  }
+  
+  export default App;
--- src/players.ts
+++ src/players.ts
@@ -2,36 +2,4 @@
 import React, { useMemo, useState, useEffect } from "react";
 import Table from "./Table";
 
-  function App() {
-   const columns = useMemo(
-      () => [
-        {
-         
-          Header: "Score Table",
-          columns: [
-            {
-              Header: "Username",
-              accessor: "show.name"
-            },
-            {
-              Header: "Points",
-              accessor: "show.points"
-
-                  );
-
-            }
-          ]
-        }
-    ],
-        []
-   );
-  
-  
-    return (
-      <div className="App">
-        <Table columns={columns} data={data} />
-      </div>
-    );
-  }
-  
-  export default App;
+ 
